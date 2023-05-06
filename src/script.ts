e4196172cde686c451023d485ca086d30ded1f0f
NataRover <vip.aleksa78@mail.ru>
2023-05-07 00:12:58 +0300

    0705

--- src/script.ts
+++ src/script.ts
@@ -18,22 +18,41 @@ window.addEventListener("scroll", function () {
 //MENU-SHOW
 const activBurgerMenu = document.querySelector(".header-burger-menu");
 const activNav = document.querySelector(".audiobook-navigation-module");
+const hiddenNav = document.querySelector(".opasity-container");
+
+function noScroll() {
+  let html = document.documentElement;
+  html.classList.add("noscroll");
+}
+function windowScroll() {
+  let html = document.documentElement;
+  html.classList.remove("noscroll");
+}
 
 activBurgerMenu?.addEventListener("click", function (e) {
-  console.log("mmkm");
+  console.log("clickBurger");
 
   if (activNav?.classList.contains("display-hidden")) {
     activNav.classList.remove("display-hidden");
+    activBurgerMenu.classList.add("display-hidden");
+    hiddenNav?.classList.add("add-bg");
+    hiddenNav?.classList.remove("display-hidden");
+    noScroll();
   } else {
     activNav?.classList.add("display-hidden");
+    activBurgerMenu.classList.remove("display-hidden");
+    hiddenNav?.classList.remove("add-bg");
+    hiddenNav?.classList.add("display-hidden");
   }
 });
-
-// MENU-HIDDEN
-const hiddenNav = document.querySelector(".opasity-container");
+// menu hidden
 hiddenNav?.addEventListener("click", function () {
   console.log("clickCont");
   activNav?.classList.add("display-hidden");
+  activBurgerMenu?.classList.remove("display-hidden");
+  hiddenNav?.classList.remove("add-bg");
+  hiddenNav?.classList.add("display-hidden");
+  windowScroll();
 });
 
 // OBSERVER
